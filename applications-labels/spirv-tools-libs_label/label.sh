8eb369fa7f7a5f8bd295e435f21eace44005f4c1
noatsecure <noatsecure@protonmail.com>
2021-08-22 16:54:18 -0600

    Generated SELinux policy module (label-only) for the 'spirv-tools-libs' package

---
+++ applications-labels/spirv-tools-libs_label/label.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+/usr/sbin/semanage fcontext --add --type spirv-tools-libs_lib_t /usr/lib/libSPIRV-Tools-link.so;
+/usr/sbin/semanage fcontext --add --type spirv-tools-libs_lib_t /usr/lib/libSPIRV-Tools-opt.so;
+/usr/sbin/semanage fcontext --add --type spirv-tools-libs_lib_t /usr/lib/libSPIRV-Tools-reduce.so;
+/usr/sbin/semanage fcontext --add --type spirv-tools-libs_lib_t /usr/lib/libSPIRV-Tools-shared.so;
+/usr/sbin/semanage fcontext --add --type spirv-tools-libs_lib_t /usr/lib/libSPIRV-Tools.so;
