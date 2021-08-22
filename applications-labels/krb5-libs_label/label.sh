9d0cd1995fdbe5f25863f2b254cc58ab4999537e
noatsecure <noatsecure@protonmail.com>
2021-08-22 16:51:27 -0600

    Generated SELinux policy module (label-only) for the 'krb5-libs' package

---
+++ applications-labels/krb5-libs_label/label.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+/usr/sbin/semanage fcontext --add --type krb5-libs_lib_t /usr/lib/krb5/plugins/preauth/spake.so;
+/usr/sbin/semanage fcontext --add --type krb5-libs_lib_t /usr/lib/krb5/plugins/tls/k5tls.so;
+/usr/sbin/semanage fcontext --add --type krb5-libs_lib_t /usr/lib/libgssapi_krb5.so.2.2;
+/usr/sbin/semanage fcontext --add --type krb5-libs_lib_t /usr/lib/libgssapi_krb5.so.2;
+/usr/sbin/semanage fcontext --add --type krb5-libs_lib_t /usr/lib/libgssrpc.so.4.2;
+/usr/sbin/semanage fcontext --add --type krb5-libs_lib_t /usr/lib/libgssrpc.so.4;
+/usr/sbin/semanage fcontext --add --type krb5-libs_lib_t /usr/lib/libk5crypto.so.3.1;
+/usr/sbin/semanage fcontext --add --type krb5-libs_lib_t /usr/lib/libk5crypto.so.3;
+/usr/sbin/semanage fcontext --add --type krb5-libs_lib_t /usr/lib/libkdb5.so.10.0;
+/usr/sbin/semanage fcontext --add --type krb5-libs_lib_t /usr/lib/libkdb5.so.10;
+/usr/sbin/semanage fcontext --add --type krb5-libs_lib_t /usr/lib/libkrad.so.0.0;
+/usr/sbin/semanage fcontext --add --type krb5-libs_lib_t /usr/lib/libkrad.so.0;
+/usr/sbin/semanage fcontext --add --type krb5-libs_lib_t /usr/lib/libkrb5.so.3.3;
+/usr/sbin/semanage fcontext --add --type krb5-libs_lib_t /usr/lib/libkrb5.so.3;
+/usr/sbin/semanage fcontext --add --type krb5-libs_lib_t /usr/lib/libkrb5support.so.0.1;
+/usr/sbin/semanage fcontext --add --type krb5-libs_lib_t /usr/lib/libkrb5support.so.0;
