fe5adb753c8e2c824234758e963968605fb6c2f8
noatsecure <noatsecure@protonmail.com>
2021-08-22 16:51:26 -0600

    Generated SELinux policy module (label-only) for the 'gettext-libs' package

---
+++ applications-labels/gettext-libs_label/label.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+/usr/sbin/semanage fcontext --add --type gettext-libs_lib_t /usr/lib/libasprintf.so.0.0.0;
+/usr/sbin/semanage fcontext --add --type gettext-libs_lib_t /usr/lib/libasprintf.so.0;
+/usr/sbin/semanage fcontext --add --type gettext-libs_lib_t /usr/lib/libgettextlib-0.21.so;
+/usr/sbin/semanage fcontext --add --type gettext-libs_lib_t /usr/lib/libgettextpo.so.0.5.7;
+/usr/sbin/semanage fcontext --add --type gettext-libs_lib_t /usr/lib/libgettextpo.so.0;
+/usr/sbin/semanage fcontext --add --type gettext-libs_lib_t /usr/lib/libgettextsrc-0.21.so;
