bb4b6e6cc325e6574707532143be3b276a0696ae
noatsecure <noatsecure@protonmail.com>
2021-08-22 16:54:17 -0600

    Generated SELinux policy module (label-only) for the 'rpm-libs' package

---
+++ applications-labels/rpm-libs_label/label.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+/usr/sbin/semanage fcontext --add --type rpm-libs_lib_t /usr/lib/librpm.so.9.1.3;
+/usr/sbin/semanage fcontext --add --type rpm-libs_lib_t /usr/lib/librpm.so.9;
+/usr/sbin/semanage fcontext --add --type rpm-libs_lib_t /usr/lib/librpmio.so.9.1.3;
+/usr/sbin/semanage fcontext --add --type rpm-libs_lib_t /usr/lib/librpmio.so.9;
