1d9d30b97ecaa1f3cab6c379d64b8a861415888c
noatsecure <noatsecure@protonmail.com>
2021-08-22 16:54:16 -0600

    Generated SELinux policy module (label-only) for the 'openssl-libs' package

---
+++ applications-labels/openssl-libs_label/label.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+/usr/sbin/semanage fcontext --add --type openssl-libs_lib_t /usr/lib/.libcrypto.so.1.1.1k.hmac;
+/usr/sbin/semanage fcontext --add --type openssl-libs_lib_t /usr/lib/.libcrypto.so.1.1.hmac;
+/usr/sbin/semanage fcontext --add --type openssl-libs_lib_t /usr/lib/.libssl.so.1.1.1k.hmac;
+/usr/sbin/semanage fcontext --add --type openssl-libs_lib_t /usr/lib/.libssl.so.1.1.hmac;
+/usr/sbin/semanage fcontext --add --type openssl-libs_lib_t /usr/lib/engines-1.1/afalg.so;
+/usr/sbin/semanage fcontext --add --type openssl-libs_lib_t /usr/lib/engines-1.1/capi.so;
+/usr/sbin/semanage fcontext --add --type openssl-libs_lib_t /usr/lib/engines-1.1/padlock.so;
+/usr/sbin/semanage fcontext --add --type openssl-libs_lib_t /usr/lib/libcrypto.so.1.1.1k;
+/usr/sbin/semanage fcontext --add --type openssl-libs_lib_t /usr/lib/libcrypto.so.1.1;
+/usr/sbin/semanage fcontext --add --type openssl-libs_lib_t /usr/lib/libssl.so.1.1.1k;
+/usr/sbin/semanage fcontext --add --type openssl-libs_lib_t /usr/lib/libssl.so.1.1;
