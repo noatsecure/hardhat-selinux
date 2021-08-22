63a74ec30f7938528e6f4d88675f524b8dfedced
noatsecure <noatsecure@protonmail.com>
2021-08-22 16:51:25 -0600

    Generated SELinux policy module (label-only) for the 'daxctl-libs' package

---
+++ applications-labels/daxctl-libs_label/label.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+/usr/sbin/semanage fcontext --add --type daxctl-libs_lib_t /usr/lib/libdaxctl.so.1.5.0;
+/usr/sbin/semanage fcontext --add --type daxctl-libs_lib_t /usr/lib/libdaxctl.so.1;
