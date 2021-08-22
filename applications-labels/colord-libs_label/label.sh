1233e89c8633521a2a381dc193dd79caaba302b4
noatsecure <noatsecure@protonmail.com>
2021-08-22 16:51:25 -0600

    Generated SELinux policy module (label-only) for the 'colord-libs' package

---
+++ applications-labels/colord-libs_label/label.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+/usr/sbin/semanage fcontext --add --type colord-libs_lib_t /usr/lib/libcolord.so.2.0.5;
+/usr/sbin/semanage fcontext --add --type colord-libs_lib_t /usr/lib/libcolord.so.2;
+/usr/sbin/semanage fcontext --add --type colord-libs_lib_t /usr/lib/libcolordcompat.so;
+/usr/sbin/semanage fcontext --add --type colord-libs_lib_t /usr/lib/libcolordprivate.so.2.0.5;
+/usr/sbin/semanage fcontext --add --type colord-libs_lib_t /usr/lib/libcolordprivate.so.2;
+/usr/sbin/semanage fcontext --add --type colord-libs_lib_t /usr/lib/libcolorhug.so.2.0.5;
+/usr/sbin/semanage fcontext --add --type colord-libs_lib_t /usr/lib/libcolorhug.so.2;
