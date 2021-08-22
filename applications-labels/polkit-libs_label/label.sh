02da51ec8f1fd429013e52619ac95f1e118c5dc8
noatsecure <noatsecure@protonmail.com>
2021-08-22 16:54:17 -0600

    Generated SELinux policy module (label-only) for the 'polkit-libs' package

---
+++ applications-labels/polkit-libs_label/label.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+/usr/sbin/semanage fcontext --add --type polkit-libs_lib_t /usr/lib/libpolkit-agent-1.so.0.0.0;
+/usr/sbin/semanage fcontext --add --type polkit-libs_lib_t /usr/lib/libpolkit-agent-1.so.0;
+/usr/sbin/semanage fcontext --add --type polkit-libs_lib_t /usr/lib/libpolkit-gobject-1.so.0.0.0;
+/usr/sbin/semanage fcontext --add --type polkit-libs_lib_t /usr/lib/libpolkit-gobject-1.so.0;
