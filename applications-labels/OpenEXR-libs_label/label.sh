cdf30a5f4257df85286b4d924244118809eba614
noatsecure <noatsecure@protonmail.com>
2021-08-22 16:51:24 -0600

    Generated SELinux policy module (label-only) for the 'OpenEXR-libs' package

---
+++ applications-labels/OpenEXR-libs_label/label.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+/usr/sbin/semanage fcontext --add --type OpenEXR-libs_lib_t /usr/lib/libIlmImf-2_3.so.24.0.0;
+/usr/sbin/semanage fcontext --add --type OpenEXR-libs_lib_t /usr/lib/libIlmImf-2_3.so.24;
+/usr/sbin/semanage fcontext --add --type OpenEXR-libs_lib_t /usr/lib/libIlmImfUtil-2_3.so.24.0.0;
+/usr/sbin/semanage fcontext --add --type OpenEXR-libs_lib_t /usr/lib/libIlmImfUtil-2_3.so.24;
