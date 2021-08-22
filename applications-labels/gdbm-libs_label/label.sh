e7cfbcf172a1c6b4a62a79627895591575adccaa
noatsecure <noatsecure@protonmail.com>
2021-08-22 16:51:26 -0600

    Generated SELinux policy module (label-only) for the 'gdbm-libs' package

---
+++ applications-labels/gdbm-libs_label/label.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+/usr/sbin/semanage fcontext --add --type gdbm-libs_lib_t /usr/lib/libgdbm.so.6.0.0;
+/usr/sbin/semanage fcontext --add --type gdbm-libs_lib_t /usr/lib/libgdbm.so.6;
+/usr/sbin/semanage fcontext --add --type gdbm-libs_lib_t /usr/lib/libgdbm_compat.so.4.0.0;
+/usr/sbin/semanage fcontext --add --type gdbm-libs_lib_t /usr/lib/libgdbm_compat.so.4;
