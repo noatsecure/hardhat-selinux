e4033734e96c18f6502421be953c9d623cb69886
noatsecure <noatsecure@protonmail.com>
2021-08-28 18:11:43 -0600

    Updated SELinux policy module for package '/home/sysadmin/hardhat-selinux/applications-labels/xz-libs_label'

--- applications-labels/xz-libs_label/label.sh
+++ applications-labels/xz-libs_label/label.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/usr/sbin/semanage fcontext --add --type xz-libs_lib_t /usr/lib/liblzma.so.5.2.5;
-/usr/sbin/semanage fcontext --add --type xz-libs_lib_t /usr/lib/liblzma.so.5;
+/usr/sbin/semanage fcontext --add --type xz-libs_t /usr/lib/liblzma.so.5.2.5;
+/usr/sbin/semanage fcontext --add --type xz-libs_t /usr/lib/liblzma.so.5;
