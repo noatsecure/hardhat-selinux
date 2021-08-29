465fbb1272c43d9e2fb4b640257d5398758f5024
noatsecure <noatsecure@protonmail.com>
2021-08-28 18:11:41 -0600

    Updated SELinux policy module for package '/home/sysadmin/hardhat-selinux/applications-labels/samba-common-libs_label'

--- applications-labels/samba-common-libs_label/label.sh
+++ applications-labels/samba-common-libs_label/label.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-/usr/sbin/semanage fcontext --add --type samba-common-libs_lib_t /usr/lib/samba/libpopt-samba3-cmdline-samba4.so;
-/usr/sbin/semanage fcontext --add --type samba-common-libs_lib_t /usr/lib/samba/libpopt-samba3-samba4.so;
-/usr/sbin/semanage fcontext --add --type samba-common-libs_lib_t /usr/lib/samba/pdb/ldapsam.so;
-/usr/sbin/semanage fcontext --add --type samba-common-libs_lib_t /usr/lib/samba/pdb/smbpasswd.so;
-/usr/sbin/semanage fcontext --add --type samba-common-libs_lib_t /usr/lib/samba/pdb/tdbsam.so;
+/usr/sbin/semanage fcontext --add --type samba-common-libs_t /usr/lib/samba/libpopt-samba3-cmdline-samba4.so;
+/usr/sbin/semanage fcontext --add --type samba-common-libs_t /usr/lib/samba/libpopt-samba3-samba4.so;
+/usr/sbin/semanage fcontext --add --type samba-common-libs_t /usr/lib/samba/pdb/ldapsam.so;
+/usr/sbin/semanage fcontext --add --type samba-common-libs_t /usr/lib/samba/pdb/smbpasswd.so;
+/usr/sbin/semanage fcontext --add --type samba-common-libs_t /usr/lib/samba/pdb/tdbsam.so;
