272ae94d395011a13cc1bc6f531323c2f700ae48
noatsecure <noatsecure@protonmail.com>
2021-08-28 18:11:21 -0600

    Updated SELinux policy module for package '/home/sysadmin/hardhat-selinux/applications-labels/efivar-libs_label'

--- applications-labels/efivar-libs_label/label.sh
+++ applications-labels/efivar-libs_label/label.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-/usr/sbin/semanage fcontext --add --type efivar-libs_lib_t /usr/lib/libefiboot.so.1.37;
-/usr/sbin/semanage fcontext --add --type efivar-libs_lib_t /usr/lib/libefiboot.so.1;
-/usr/sbin/semanage fcontext --add --type efivar-libs_lib_t /usr/lib/libefivar.so.1.37;
-/usr/sbin/semanage fcontext --add --type efivar-libs_lib_t /usr/lib/libefivar.so.1;
+/usr/sbin/semanage fcontext --add --type efivar-libs_t /usr/lib/libefiboot.so.1.37;
+/usr/sbin/semanage fcontext --add --type efivar-libs_t /usr/lib/libefiboot.so.1;
+/usr/sbin/semanage fcontext --add --type efivar-libs_t /usr/lib/libefivar.so.1.37;
+/usr/sbin/semanage fcontext --add --type efivar-libs_t /usr/lib/libefivar.so.1;
