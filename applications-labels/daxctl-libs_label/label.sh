6a1f59cdd525fa92b4d989ba95e51f3748dbcd71
noatsecure <noatsecure@protonmail.com>
2021-08-28 18:11:21 -0600

    Updated SELinux policy module for package '/home/sysadmin/hardhat-selinux/applications-labels/daxctl-libs_label'

--- applications-labels/daxctl-libs_label/label.sh
+++ applications-labels/daxctl-libs_label/label.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/usr/sbin/semanage fcontext --add --type daxctl-libs_lib_t /usr/lib/libdaxctl.so.1.5.0;
-/usr/sbin/semanage fcontext --add --type daxctl-libs_lib_t /usr/lib/libdaxctl.so.1;
+/usr/sbin/semanage fcontext --add --type daxctl-libs_t /usr/lib/libdaxctl.so.1.5.0;
+/usr/sbin/semanage fcontext --add --type daxctl-libs_t /usr/lib/libdaxctl.so.1;
