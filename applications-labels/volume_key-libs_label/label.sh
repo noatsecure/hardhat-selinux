deff402f15a5985be28fa363d20b146665c73c69
noatsecure <noatsecure@protonmail.com>
2021-08-28 18:11:42 -0600

    Updated SELinux policy module for package '/home/sysadmin/hardhat-selinux/applications-labels/volume_key-libs_label'

--- applications-labels/volume_key-libs_label/label.sh
+++ applications-labels/volume_key-libs_label/label.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/usr/sbin/semanage fcontext --add --type volume_key-libs_lib_t /usr/lib/libvolume_key.so.1.2.3;
-/usr/sbin/semanage fcontext --add --type volume_key-libs_lib_t /usr/lib/libvolume_key.so.1;
+/usr/sbin/semanage fcontext --add --type volume_key-libs_t /usr/lib/libvolume_key.so.1.2.3;
+/usr/sbin/semanage fcontext --add --type volume_key-libs_t /usr/lib/libvolume_key.so.1;
