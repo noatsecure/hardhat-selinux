e977b82f6161aaadf658f08b255f634d3e24cac2
noatsecure <noatsecure@protonmail.com>
2021-08-28 18:11:20 -0600

    Updated SELinux policy module for package '/home/sysadmin/hardhat-selinux/applications-labels/authselect-libs_label'

--- applications-labels/authselect-libs_label/label.sh
+++ applications-labels/authselect-libs_label/label.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/usr/sbin/semanage fcontext --add --type authselect-libs_lib_t /usr/lib/libauthselect.so.3.1.0;
-/usr/sbin/semanage fcontext --add --type authselect-libs_lib_t /usr/lib/libauthselect.so.3;
+/usr/sbin/semanage fcontext --add --type authselect-libs_t /usr/lib/libauthselect.so.3.1.0;
+/usr/sbin/semanage fcontext --add --type authselect-libs_t /usr/lib/libauthselect.so.3;
