593303ce900088056c1ea68aeecb53674a0f2b76
noatsecure <noatsecure@protonmail.com>
2021-08-28 18:11:23 -0600

    Updated SELinux policy module for package '/home/sysadmin/hardhat-selinux/applications-labels/keyutils-libs_label'

--- applications-labels/keyutils-libs_label/label.sh
+++ applications-labels/keyutils-libs_label/label.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/usr/sbin/semanage fcontext --add --type keyutils-libs_lib_t /usr/lib/libkeyutils.so.1.9;
-/usr/sbin/semanage fcontext --add --type keyutils-libs_lib_t /usr/lib/libkeyutils.so.1;
+/usr/sbin/semanage fcontext --add --type keyutils-libs_t /usr/lib/libkeyutils.so.1.9;
+/usr/sbin/semanage fcontext --add --type keyutils-libs_t /usr/lib/libkeyutils.so.1;
