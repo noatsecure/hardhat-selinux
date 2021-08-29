5a39bf2df24947180ea710d5fbf724f493918290
noatsecure <noatsecure@protonmail.com>
2021-08-28 18:11:23 -0600

    Updated SELinux policy module for package '/home/sysadmin/hardhat-selinux/applications-labels/jasper-libs_label'

--- applications-labels/jasper-libs_label/label.sh
+++ applications-labels/jasper-libs_label/label.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/usr/sbin/semanage fcontext --add --type jasper-libs_lib_t /usr/lib/libjasper.so.4.0.0;
-/usr/sbin/semanage fcontext --add --type jasper-libs_lib_t /usr/lib/libjasper.so.4;
+/usr/sbin/semanage fcontext --add --type jasper-libs_t /usr/lib/libjasper.so.4.0.0;
+/usr/sbin/semanage fcontext --add --type jasper-libs_t /usr/lib/libjasper.so.4;
