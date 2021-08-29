b4184247aafbfbc878320cc501162fd366a6c4e3
noatsecure <noatsecure@protonmail.com>
2021-08-28 18:11:41 -0600

    Updated SELinux policy module for package '/home/sysadmin/hardhat-selinux/applications-labels/qrencode-libs_label'

--- applications-labels/qrencode-libs_label/label.sh
+++ applications-labels/qrencode-libs_label/label.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/usr/sbin/semanage fcontext --add --type qrencode-libs_lib_t /usr/lib/libqrencode.so.4.0.2;
-/usr/sbin/semanage fcontext --add --type qrencode-libs_lib_t /usr/lib/libqrencode.so.4;
+/usr/sbin/semanage fcontext --add --type qrencode-libs_t /usr/lib/libqrencode.so.4.0.2;
+/usr/sbin/semanage fcontext --add --type qrencode-libs_t /usr/lib/libqrencode.so.4;
