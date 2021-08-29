dd8fed8062354aeef0eb864eef8fc00aea627809
noatsecure <noatsecure@protonmail.com>
2021-08-28 18:11:21 -0600

    Updated SELinux policy module for package '/home/sysadmin/hardhat-selinux/applications-labels/e2fsprogs-libs_label'

--- applications-labels/e2fsprogs-libs_label/label.sh
+++ applications-labels/e2fsprogs-libs_label/label.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-/usr/sbin/semanage fcontext --add --type e2fsprogs-libs_lib_t /usr/lib/libe2p.so.2.3;
-/usr/sbin/semanage fcontext --add --type e2fsprogs-libs_lib_t /usr/lib/libe2p.so.2;
-/usr/sbin/semanage fcontext --add --type e2fsprogs-libs_lib_t /usr/lib/libext2fs.so.2.4;
-/usr/sbin/semanage fcontext --add --type e2fsprogs-libs_lib_t /usr/lib/libext2fs.so.2;
+/usr/sbin/semanage fcontext --add --type e2fsprogs-libs_t /usr/lib/libe2p.so.2.3;
+/usr/sbin/semanage fcontext --add --type e2fsprogs-libs_t /usr/lib/libe2p.so.2;
+/usr/sbin/semanage fcontext --add --type e2fsprogs-libs_t /usr/lib/libext2fs.so.2.4;
+/usr/sbin/semanage fcontext --add --type e2fsprogs-libs_t /usr/lib/libext2fs.so.2;
