ff2f7279ecc4dd975abff50b0ed8901db3a9e414
noatsecure <noatsecure@protonmail.com>
2021-08-28 18:11:21 -0600

    Updated SELinux policy module for package '/home/sysadmin/hardhat-selinux/applications-labels/cryptsetup-libs_label'

--- applications-labels/cryptsetup-libs_label/label.sh
+++ applications-labels/cryptsetup-libs_label/label.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/usr/sbin/semanage fcontext --add --type cryptsetup-libs_lib_t /usr/lib/libcryptsetup.so.12.6.0;
-/usr/sbin/semanage fcontext --add --type cryptsetup-libs_lib_t /usr/lib/libcryptsetup.so.12;
+/usr/sbin/semanage fcontext --add --type cryptsetup-libs_t /usr/lib/libcryptsetup.so.12.6.0;
+/usr/sbin/semanage fcontext --add --type cryptsetup-libs_t /usr/lib/libcryptsetup.so.12;
