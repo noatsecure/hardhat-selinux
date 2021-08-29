0d1233d3fad8851c9ad37ea3e39b6bda433a0d6f
noatsecure <noatsecure@protonmail.com>
2021-08-28 18:11:21 -0600

    Updated SELinux policy module for package '/home/sysadmin/hardhat-selinux/applications-labels/device-mapper-libs_label'

--- applications-labels/device-mapper-libs_label/label.sh
+++ applications-labels/device-mapper-libs_label/label.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/sbin/semanage fcontext --add --type device-mapper-libs_lib_t /usr/lib/libdevmapper.so.1.02;
+/usr/sbin/semanage fcontext --add --type device-mapper-libs_t /usr/lib/libdevmapper.so.1.02;
