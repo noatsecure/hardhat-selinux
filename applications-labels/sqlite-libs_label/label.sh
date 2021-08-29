8fda61bb23ec17d271fa061dc293821fe691d3a7
noatsecure <noatsecure@protonmail.com>
2021-08-28 18:11:42 -0600

    Updated SELinux policy module for package '/home/sysadmin/hardhat-selinux/applications-labels/sqlite-libs_label'

--- applications-labels/sqlite-libs_label/label.sh
+++ applications-labels/sqlite-libs_label/label.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/usr/sbin/semanage fcontext --add --type sqlite-libs_lib_t /usr/lib/libsqlite3.so.0.8.6;
-/usr/sbin/semanage fcontext --add --type sqlite-libs_lib_t /usr/lib/libsqlite3.so.0;
+/usr/sbin/semanage fcontext --add --type sqlite-libs_t /usr/lib/libsqlite3.so.0.8.6;
+/usr/sbin/semanage fcontext --add --type sqlite-libs_t /usr/lib/libsqlite3.so.0;
