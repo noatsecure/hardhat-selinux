c38d4123cb21f5bcbd5c7bfe684199041197f8ec
noatsecure <noatsecure@protonmail.com>
2021-08-28 18:11:20 -0600

    Updated SELinux policy module for package '/home/sysadmin/hardhat-selinux/applications-labels/audit-libs_label'

--- applications-labels/audit-libs_label/label.sh
+++ applications-labels/audit-libs_label/label.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/usr/sbin/semanage fcontext --add --type audit-libs_lib_t /usr/lib/libaudit.so.1.0.0;
-/usr/sbin/semanage fcontext --add --type audit-libs_lib_t /usr/lib/libauparse.so.0.0.0;
+/usr/sbin/semanage fcontext --add --type audit-libs_t /usr/lib/libaudit.so.1.0.0;
+/usr/sbin/semanage fcontext --add --type audit-libs_t /usr/lib/libauparse.so.0.0.0;
