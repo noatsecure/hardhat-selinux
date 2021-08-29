e0ec715d46712bd76657e34d3b148da06ab93015
noatsecure <noatsecure@protonmail.com>
2021-08-28 18:11:40 -0600

    Updated SELinux policy module for package '/home/sysadmin/hardhat-selinux/applications-labels/ndctl-libs_label'

--- applications-labels/ndctl-libs_label/label.sh
+++ applications-labels/ndctl-libs_label/label.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/usr/sbin/semanage fcontext --add --type ndctl-libs_lib_t /usr/lib/libndctl.so.6.19.1;
-/usr/sbin/semanage fcontext --add --type ndctl-libs_lib_t /usr/lib/libndctl.so.6;
+/usr/sbin/semanage fcontext --add --type ndctl-libs_t /usr/lib/libndctl.so.6.19.1;
+/usr/sbin/semanage fcontext --add --type ndctl-libs_t /usr/lib/libndctl.so.6;
