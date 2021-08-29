aae819a7402b337a2118f57658e51d5628e59bef
noatsecure <noatsecure@protonmail.com>
2021-08-28 18:11:39 -0600

    Updated SELinux policy module for package '/home/sysadmin/hardhat-selinux/applications-labels/linux-atm-libs_label'

--- applications-labels/linux-atm-libs_label/label.sh
+++ applications-labels/linux-atm-libs_label/label.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/usr/sbin/semanage fcontext --add --type linux-atm-libs_lib_t /usr/lib/libatm.so.1.0.0;
-/usr/sbin/semanage fcontext --add --type linux-atm-libs_lib_t /usr/lib/libatm.so.1;
+/usr/sbin/semanage fcontext --add --type linux-atm-libs_t /usr/lib/libatm.so.1.0.0;
+/usr/sbin/semanage fcontext --add --type linux-atm-libs_t /usr/lib/libatm.so.1;
