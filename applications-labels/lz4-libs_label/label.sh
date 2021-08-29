cc9934c8eb04ad74fa276e59d49da9de68204ef7
noatsecure <noatsecure@protonmail.com>
2021-08-28 18:11:39 -0600

    Updated SELinux policy module for package '/home/sysadmin/hardhat-selinux/applications-labels/lz4-libs_label'

--- applications-labels/lz4-libs_label/label.sh
+++ applications-labels/lz4-libs_label/label.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/usr/sbin/semanage fcontext --add --type lz4-libs_lib_t /usr/lib/liblz4.so.1.9.1;
-/usr/sbin/semanage fcontext --add --type lz4-libs_lib_t /usr/lib/liblz4.so.1;
+/usr/sbin/semanage fcontext --add --type lz4-libs_t /usr/lib/liblz4.so.1.9.1;
+/usr/sbin/semanage fcontext --add --type lz4-libs_t /usr/lib/liblz4.so.1;
