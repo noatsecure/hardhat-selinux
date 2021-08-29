f7d6986573892aa7491f2a354d41fb38ad1d5fd0
noatsecure <noatsecure@protonmail.com>
2021-08-28 18:11:42 -0600

    Updated SELinux policy module for package '/home/sysadmin/hardhat-selinux/applications-labels/unbound-libs_label'

--- applications-labels/unbound-libs_label/label.sh
+++ applications-labels/unbound-libs_label/label.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/usr/sbin/semanage fcontext --add --type unbound-libs_lib_t /usr/lib/libunbound.so.8.1.12;
-/usr/sbin/semanage fcontext --add --type unbound-libs_lib_t /usr/lib/libunbound.so.8;
+/usr/sbin/semanage fcontext --add --type unbound-libs_t /usr/lib/libunbound.so.8.1.12;
+/usr/sbin/semanage fcontext --add --type unbound-libs_t /usr/lib/libunbound.so.8;
