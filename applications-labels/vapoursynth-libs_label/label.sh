dca9e3a467834f8fc63c61fc99abc80a4d2e34a5
noatsecure <noatsecure@protonmail.com>
2021-08-28 18:11:42 -0600

    Updated SELinux policy module for package '/home/sysadmin/hardhat-selinux/applications-labels/vapoursynth-libs_label'

--- applications-labels/vapoursynth-libs_label/label.sh
+++ applications-labels/vapoursynth-libs_label/label.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-/usr/sbin/semanage fcontext --add --type vapoursynth-libs_lib_t /usr/lib/libvapoursynth-script.so.0.0.0;
-/usr/sbin/semanage fcontext --add --type vapoursynth-libs_lib_t /usr/lib/libvapoursynth-script.so.0;
-/usr/sbin/semanage fcontext --add --type vapoursynth-libs_lib_t /usr/lib/libvapoursynth.so.51.0.0;
-/usr/sbin/semanage fcontext --add --type vapoursynth-libs_lib_t /usr/lib/libvapoursynth.so.51;
+/usr/sbin/semanage fcontext --add --type vapoursynth-libs_t /usr/lib/libvapoursynth-script.so.0.0.0;
+/usr/sbin/semanage fcontext --add --type vapoursynth-libs_t /usr/lib/libvapoursynth-script.so.0;
+/usr/sbin/semanage fcontext --add --type vapoursynth-libs_t /usr/lib/libvapoursynth.so.51.0.0;
+/usr/sbin/semanage fcontext --add --type vapoursynth-libs_t /usr/lib/libvapoursynth.so.51;
