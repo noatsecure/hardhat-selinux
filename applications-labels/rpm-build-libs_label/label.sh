6127acec36115c7ee40c9890db3a440c8bbded21
noatsecure <noatsecure@protonmail.com>
2021-08-28 18:11:41 -0600

    Updated SELinux policy module for package '/home/sysadmin/hardhat-selinux/applications-labels/rpm-build-libs_label'

--- applications-labels/rpm-build-libs_label/label.sh
+++ applications-labels/rpm-build-libs_label/label.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/usr/sbin/semanage fcontext --add --type rpm-build-libs_lib_t /usr/lib/librpmbuild.so.9.1.3;
-/usr/sbin/semanage fcontext --add --type rpm-build-libs_lib_t /usr/lib/librpmbuild.so.9;
+/usr/sbin/semanage fcontext --add --type rpm-build-libs_t /usr/lib/librpmbuild.so.9.1.3;
+/usr/sbin/semanage fcontext --add --type rpm-build-libs_t /usr/lib/librpmbuild.so.9;
