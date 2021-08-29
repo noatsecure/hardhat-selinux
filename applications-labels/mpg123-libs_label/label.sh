a110adee9c5985394d664d837f7b250c8294f79d
noatsecure <noatsecure@protonmail.com>
2021-08-28 18:11:40 -0600

    Updated SELinux policy module for package '/home/sysadmin/hardhat-selinux/applications-labels/mpg123-libs_label'

--- applications-labels/mpg123-libs_label/label.sh
+++ applications-labels/mpg123-libs_label/label.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-/usr/sbin/semanage fcontext --add --type mpg123-libs_lib_t /usr/lib/libout123.so.0.3.0;
-/usr/sbin/semanage fcontext --add --type mpg123-libs_lib_t /usr/lib/libout123.so.0;
-/usr/sbin/semanage fcontext --add --type mpg123-libs_lib_t /usr/lib/libsyn123.so.0.1.1;
-/usr/sbin/semanage fcontext --add --type mpg123-libs_lib_t /usr/lib/libsyn123.so.0;
+/usr/sbin/semanage fcontext --add --type mpg123-libs_t /usr/lib/libout123.so.0.3.0;
+/usr/sbin/semanage fcontext --add --type mpg123-libs_t /usr/lib/libout123.so.0;
+/usr/sbin/semanage fcontext --add --type mpg123-libs_t /usr/lib/libsyn123.so.0.1.1;
+/usr/sbin/semanage fcontext --add --type mpg123-libs_t /usr/lib/libsyn123.so.0;
