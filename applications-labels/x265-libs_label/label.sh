4f9fb38e9a66ce832e1b95123654606d098c71bd
noatsecure <noatsecure@protonmail.com>
2021-08-28 18:11:42 -0600

    Updated SELinux policy module for package '/home/sysadmin/hardhat-selinux/applications-labels/x265-libs_label'

--- applications-labels/x265-libs_label/label.sh
+++ applications-labels/x265-libs_label/label.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-/usr/sbin/semanage fcontext --add --type x265-libs_lib_t /usr/lib/libx265.so.192;
-/usr/sbin/semanage fcontext --add --type x265-libs_lib_t /usr/lib/libx265_main10.so.192;
-/usr/sbin/semanage fcontext --add --type x265-libs_lib_t /usr/lib/libx265_main12.so.192;
+/usr/sbin/semanage fcontext --add --type x265-libs_t /usr/lib/libx265.so.192;
+/usr/sbin/semanage fcontext --add --type x265-libs_t /usr/lib/libx265_main10.so.192;
+/usr/sbin/semanage fcontext --add --type x265-libs_t /usr/lib/libx265_main12.so.192;
