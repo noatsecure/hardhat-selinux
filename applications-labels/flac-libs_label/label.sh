d267abfca9d4d0a0278e16e6b63c51b1efeb64ac
noatsecure <noatsecure@protonmail.com>
2021-08-28 18:11:22 -0600

    Updated SELinux policy module for package '/home/sysadmin/hardhat-selinux/applications-labels/flac-libs_label'

--- applications-labels/flac-libs_label/label.sh
+++ applications-labels/flac-libs_label/label.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/usr/sbin/semanage fcontext --add --type flac-libs_lib_t /usr/lib/libFLAC++.so.6.3.0;
-/usr/sbin/semanage fcontext --add --type flac-libs_lib_t /usr/lib/libFLAC++.so.6;
+/usr/sbin/semanage fcontext --add --type flac-libs_t /usr/lib/libFLAC++.so.6.3.0;
+/usr/sbin/semanage fcontext --add --type flac-libs_t /usr/lib/libFLAC++.so.6;
