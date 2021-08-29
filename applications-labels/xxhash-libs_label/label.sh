55bfb1802297a48514cf2a32f440e308332748b8
noatsecure <noatsecure@protonmail.com>
2021-08-28 18:11:42 -0600

    Updated SELinux policy module for package '/home/sysadmin/hardhat-selinux/applications-labels/xxhash-libs_label'

--- applications-labels/xxhash-libs_label/label.sh
+++ applications-labels/xxhash-libs_label/label.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/usr/sbin/semanage fcontext --add --type xxhash-libs_lib_t /usr/lib/libxxhash.so.0.8.0;
-/usr/sbin/semanage fcontext --add --type xxhash-libs_lib_t /usr/lib/libxxhash.so.0;
+/usr/sbin/semanage fcontext --add --type xxhash-libs_t /usr/lib/libxxhash.so.0.8.0;
+/usr/sbin/semanage fcontext --add --type xxhash-libs_t /usr/lib/libxxhash.so.0;
