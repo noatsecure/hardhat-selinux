2cea8e0cfd8768c6cd80063901a41965d9aae6d4
noatsecure <noatsecure@protonmail.com>
2021-08-28 18:11:22 -0600

    Updated SELinux policy module for package '/home/sysadmin/hardhat-selinux/applications-labels/ipset-libs_label'

--- applications-labels/ipset-libs_label/label.sh
+++ applications-labels/ipset-libs_label/label.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/usr/sbin/semanage fcontext --add --type ipset-libs_lib_t /usr/lib/libipset.so.13.1.0;
-/usr/sbin/semanage fcontext --add --type ipset-libs_lib_t /usr/lib/libipset.so.13;
+/usr/sbin/semanage fcontext --add --type ipset-libs_t /usr/lib/libipset.so.13.1.0;
+/usr/sbin/semanage fcontext --add --type ipset-libs_t /usr/lib/libipset.so.13;
