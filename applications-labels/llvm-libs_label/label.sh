f2efc06a994901a3e285c1b25280d4588b6a2a0e
noatsecure <noatsecure@protonmail.com>
2021-08-28 18:11:39 -0600

    Updated SELinux policy module for package '/home/sysadmin/hardhat-selinux/applications-labels/llvm-libs_label'

--- applications-labels/llvm-libs_label/label.sh
+++ applications-labels/llvm-libs_label/label.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-/usr/sbin/semanage fcontext --add --type llvm-libs_lib_t /usr/lib/LLVMgold.so;
-/usr/sbin/semanage fcontext --add --type llvm-libs_lib_t /usr/lib/bfd-plugins/LLVMgold.so;
-/usr/sbin/semanage fcontext --add --type llvm-libs_lib_t /usr/lib/libLLVM-11.0.0.so;
-/usr/sbin/semanage fcontext --add --type llvm-libs_lib_t /usr/lib/libLLVM-11.so;
-/usr/sbin/semanage fcontext --add --type llvm-libs_lib_t /usr/lib/libLTO.so.11;
-/usr/sbin/semanage fcontext --add --type llvm-libs_lib_t /usr/lib/libLTO.so;
-/usr/sbin/semanage fcontext --add --type llvm-libs_lib_t /usr/lib/libRemarks.so.11;
-/usr/sbin/semanage fcontext --add --type llvm-libs_lib_t /usr/lib/libRemarks.so;
+/usr/sbin/semanage fcontext --add --type llvm-libs_t /usr/lib/LLVMgold.so;
+/usr/sbin/semanage fcontext --add --type llvm-libs_t /usr/lib/bfd-plugins/LLVMgold.so;
+/usr/sbin/semanage fcontext --add --type llvm-libs_t /usr/lib/libLLVM-11.0.0.so;
+/usr/sbin/semanage fcontext --add --type llvm-libs_t /usr/lib/libLLVM-11.so;
+/usr/sbin/semanage fcontext --add --type llvm-libs_t /usr/lib/libLTO.so.11;
+/usr/sbin/semanage fcontext --add --type llvm-libs_t /usr/lib/libLTO.so;
+/usr/sbin/semanage fcontext --add --type llvm-libs_t /usr/lib/libRemarks.so.11;
+/usr/sbin/semanage fcontext --add --type llvm-libs_t /usr/lib/libRemarks.so;
