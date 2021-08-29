4279812c06b829330bb249d5d3b58e83c128dcd2
noatsecure <noatsecure@protonmail.com>
2021-08-28 18:11:41 -0600

    Updated SELinux policy module for package '/home/sysadmin/hardhat-selinux/applications-labels/pulseaudio-libs_label'

--- applications-labels/pulseaudio-libs_label/label.sh
+++ applications-labels/pulseaudio-libs_label/label.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-/usr/sbin/semanage fcontext --add --type pulseaudio-libs_lib_t /usr/lib/libpulse-simple.so.0.1.1;
-/usr/sbin/semanage fcontext --add --type pulseaudio-libs_lib_t /usr/lib/libpulse-simple.so.0;
-/usr/sbin/semanage fcontext --add --type pulseaudio-libs_lib_t /usr/lib/libpulse.so.0.23.0;
-/usr/sbin/semanage fcontext --add --type pulseaudio-libs_lib_t /usr/lib/libpulse.so.0;
-/usr/sbin/semanage fcontext --add --type pulseaudio-libs_lib_t /usr/lib/pulseaudio/libpulsecommon-14.0.so;
-/usr/sbin/semanage fcontext --add --type pulseaudio-libs_lib_t /usr/lib/pulseaudio/libpulsedsp.so;
+/usr/sbin/semanage fcontext --add --type pulseaudio-libs_t /usr/lib/libpulse-simple.so.0.1.1;
+/usr/sbin/semanage fcontext --add --type pulseaudio-libs_t /usr/lib/libpulse-simple.so.0;
+/usr/sbin/semanage fcontext --add --type pulseaudio-libs_t /usr/lib/libpulse.so.0.23.0;
+/usr/sbin/semanage fcontext --add --type pulseaudio-libs_t /usr/lib/libpulse.so.0;
+/usr/sbin/semanage fcontext --add --type pulseaudio-libs_t /usr/lib/pulseaudio/libpulsecommon-14.0.so;
+/usr/sbin/semanage fcontext --add --type pulseaudio-libs_t /usr/lib/pulseaudio/libpulsedsp.so;
