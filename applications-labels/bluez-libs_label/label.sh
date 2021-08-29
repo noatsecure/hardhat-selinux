45b935e28217170085aa77314d84463dc51c834b
noatsecure <noatsecure@protonmail.com>
2021-08-28 18:11:20 -0600

    Updated SELinux policy module for package '/home/sysadmin/hardhat-selinux/applications-labels/bluez-libs_label'

--- applications-labels/bluez-libs_label/label.sh
+++ applications-labels/bluez-libs_label/label.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/usr/sbin/semanage fcontext --add --type bluez-libs_lib_t /usr/lib/libbluetooth.so.3.19.5;
-/usr/sbin/semanage fcontext --add --type bluez-libs_lib_t /usr/lib/libbluetooth.so.3;
+/usr/sbin/semanage fcontext --add --type bluez-libs_t /usr/lib/libbluetooth.so.3.19.5;
+/usr/sbin/semanage fcontext --add --type bluez-libs_t /usr/lib/libbluetooth.so.3;
