a7d7a63df2319bf468ff17d1eeb04c1d833c0391
noatsecure <noatsecure@protonmail.com>
2021-09-04 17:03:00 -0600

    Added SELinux policy module (label only) for package 'intel-mediasdk'

---
+++ applications-labels/intel-mediasdk_label/label.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+/usr/sbin/semanage fcontext --add --type intel-mediasdk_lib_t /usr/lib/libmfx.so.1.34;
+/usr/sbin/semanage fcontext --add --type intel-mediasdk_lib_t /usr/lib/libmfx.so.1;
+/usr/sbin/semanage fcontext --add --type intel-mediasdk_lib_t /usr/lib/libmfxhw64.so.1.34;
+/usr/sbin/semanage fcontext --add --type intel-mediasdk_lib_t /usr/lib/libmfxhw64.so.1;
+/usr/sbin/semanage fcontext --add --type intel-mediasdk_lib_t /usr/lib/mfx/libmfx_h264la_hw64.so;
+/usr/sbin/semanage fcontext --add --type intel-mediasdk_lib_t /usr/lib/mfx/libmfx_hevc_fei_hw64.so;
+/usr/sbin/semanage fcontext --add --type intel-mediasdk_lib_t /usr/lib/mfx/libmfx_hevcd_hw64.so;
+/usr/sbin/semanage fcontext --add --type intel-mediasdk_lib_t /usr/lib/mfx/libmfx_hevce_hw64.so;
+/usr/sbin/semanage fcontext --add --type intel-mediasdk_lib_t /usr/lib/mfx/libmfx_vp8d_hw64.so;
+/usr/sbin/semanage fcontext --add --type intel-mediasdk_lib_t /usr/lib/mfx/libmfx_vp9d_hw64.so;
+/usr/sbin/semanage fcontext --add --type intel-mediasdk_lib_t /usr/lib/mfx/libmfx_vp9e_hw64.so;
