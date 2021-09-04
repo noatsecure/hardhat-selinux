b0d14684bec593e769320e9cc4619170bcb3a43e
noatsecure <noatsecure@protonmail.com>
2021-09-04 17:34:01 -0600

    Renamed these SELinux policy modules since their names are not valid

--- applications-labels/gcc-c++_label/label.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-/usr/sbin/semanage fcontext --add --type gcc-c++_bin_t /usr/bin/c++;
-/usr/sbin/semanage fcontext --add --type gcc-c++_bin_t /usr/bin/g++;
-/usr/sbin/semanage fcontext --add --type gcc-c++_bin_t /usr/bin/x86_64-redhat-linux-c++;
-/usr/sbin/semanage fcontext --add --type gcc-c++_bin_t /usr/bin/x86_64-redhat-linux-g++;
-/usr/sbin/semanage fcontext --add --type gcc-c++_lib_t /usr/lib/gcc/x86_64-redhat-linux/10/libstdc++.so;
-/usr/sbin/semanage fcontext --add --type gcc-c++_lib_t /usr/lib/libstdc++.so.6.0.28;
--- applications-labels/vid.stab_label/label.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-/usr/sbin/semanage fcontext --add --type vid.stab_lib_t /usr/lib/libvidstab.so.1.1;
