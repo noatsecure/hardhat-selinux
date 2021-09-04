9f93b9b327fff12fad28a7d11c3dc9e9fdeed689
noatsecure <noatsecure@protonmail.com>
2021-09-04 17:02:58 -0600

    Added SELinux policy module (label only) for package 'groff-base'

---
+++ applications-labels/groff-base_label/label.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+/usr/sbin/semanage fcontext --add --type groff-base_bin_t /usr/bin/eqn;
+/usr/sbin/semanage fcontext --add --type groff-base_bin_t /usr/bin/geqn;
+/usr/sbin/semanage fcontext --add --type groff-base_bin_t /usr/bin/gneqn;
+/usr/sbin/semanage fcontext --add --type groff-base_bin_t /usr/bin/gnroff;
+/usr/sbin/semanage fcontext --add --type groff-base_bin_t /usr/bin/gpic;
+/usr/sbin/semanage fcontext --add --type groff-base_bin_t /usr/bin/groff;
+/usr/sbin/semanage fcontext --add --type groff-base_bin_t /usr/bin/grops;
+/usr/sbin/semanage fcontext --add --type groff-base_bin_t /usr/bin/grotty;
+/usr/sbin/semanage fcontext --add --type groff-base_bin_t /usr/bin/gsoelim;
+/usr/sbin/semanage fcontext --add --type groff-base_bin_t /usr/bin/gtbl;
+/usr/sbin/semanage fcontext --add --type groff-base_bin_t /usr/bin/gtroff;
+/usr/sbin/semanage fcontext --add --type groff-base_bin_t /usr/bin/neqn;
+/usr/sbin/semanage fcontext --add --type groff-base_bin_t /usr/bin/nroff;
+/usr/sbin/semanage fcontext --add --type groff-base_bin_t /usr/bin/pic;
+/usr/sbin/semanage fcontext --add --type groff-base_bin_t /usr/bin/post-grohtml;
+/usr/sbin/semanage fcontext --add --type groff-base_bin_t /usr/bin/pre-grohtml;
+/usr/sbin/semanage fcontext --add --type groff-base_bin_t /usr/bin/preconv;
+/usr/sbin/semanage fcontext --add --type groff-base_bin_t /usr/bin/soelim.groff;
+/usr/sbin/semanage fcontext --add --type groff-base_bin_t /usr/bin/soelim;
+/usr/sbin/semanage fcontext --add --type groff-base_bin_t /usr/bin/tbl;
+/usr/sbin/semanage fcontext --add --type groff-base_bin_t /usr/bin/troff;
+/usr/sbin/semanage fcontext --add --type groff-base_bin_t /usr/bin/zsoelim;
