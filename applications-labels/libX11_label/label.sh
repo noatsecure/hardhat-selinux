ad2abf6dfcb3cb32e3e7a2f3c5bd3f15889f4dfa
noatsecure <noatsecure@protonmail.com>
2021-09-05 13:18:08 -0600

    Added previously unlabelled library

--- applications-labels/libX11_label/label.sh
+++ applications-labels/libX11_label/label.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 /usr/sbin/semanage fcontext --add --type libX11_lib_t /usr/lib/libX11.so.6.3.0;
+/usr/sbin/semanage fcontext --add --type libX11_lib_t /usr/lib/libX11.so.6.4.0;
 /usr/sbin/semanage fcontext --add --type libX11_lib_t /usr/lib/libX11.so.6;
