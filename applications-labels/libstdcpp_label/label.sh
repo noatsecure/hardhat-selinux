b24746ece25e059641402855ad5a2591e5664872
noatsecure <noatsecure@protonmail.com>
2021-09-05 12:03:13 -0600

    Updated label to account for the '+' symbols

--- applications-labels/libstdcpp_label/label.sh
+++ applications-labels/libstdcpp_label/label.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/usr/sbin/semanage fcontext --add --type libstdcpp_lib_t /usr/lib/libstdc++.so.6.0.28;
-/usr/sbin/semanage fcontext --add --type libstdcpp_lib_t /usr/lib/libstdc++.so.6;
+/usr/sbin/semanage fcontext --add --type libstdcpp_lib_t '/usr/lib/libstdc\+\+.so.6.0.28';
+/usr/sbin/semanage fcontext --add --type libstdcpp_lib_t '/usr/lib/libstdc\+\+.so.6';
