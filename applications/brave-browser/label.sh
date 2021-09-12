f71dee72ab0080b68809084e885c8023622e87e1
noatsecure <noatsecure@protonmail.com>
2021-09-12 13:14:59 -0600

    Labeled the binaries within '/usr/bin' and modified SELinux policy to allow these symlinks to work

--- applications/brave-browser/label.sh
+++ applications/brave-browser/label.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 /usr/sbin/semanage fcontext --add --type brave-browser_exec_t /opt/brave.com/brave/brave-browser;
+/usr/sbin/semanage fcontext --add --type brave-browser_exec_t /usr/bin/brave-browser;
+/usr/sbin/semanage fcontext --add --type brave-browser_exec_t /usr/bin/brave-browser-stable;
