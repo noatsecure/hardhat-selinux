8ae4392b6bc9848fed127483157959bc62709381
noatsecure <noatsecure@protonmail.com>
2021-09-11 18:27:14 -0600

    Updated SELinux policies and other labels

--- applications-labels/c-ares_label/label.sh
+++ applications-labels/c-ares_label/label.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 /usr/sbin/semanage fcontext --add --type c-ares_lib_t /usr/lib/libcares.so.2.4.2;
+/usr/sbin/semanage fcontext --add --type c-ares_lib_t /usr/lib/libcares.so.2.4.3;
 /usr/sbin/semanage fcontext --add --type c-ares_lib_t /usr/lib/libcares.so.2;
--- applications-labels/libwebp_label/label.sh
+++ applications-labels/libwebp_label/label.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 /usr/sbin/semanage fcontext --add --type libwebp_lib_t /usr/lib/libwebp.so.7.1.1;
+/usr/sbin/semanage fcontext --add --type libwebp_lib_t /usr/lib/libwebp.so.7.1.2;
 /usr/sbin/semanage fcontext --add --type libwebp_lib_t /usr/lib/libwebp.so.7;
 /usr/sbin/semanage fcontext --add --type libwebp_lib_t /usr/lib/libwebpdecoder.so.3.1.1;
+/usr/sbin/semanage fcontext --add --type libwebp_lib_t /usr/lib/libwebpdecoder.so.3.1.2;
 /usr/sbin/semanage fcontext --add --type libwebp_lib_t /usr/lib/libwebpdecoder.so.3;
 /usr/sbin/semanage fcontext --add --type libwebp_lib_t /usr/lib/libwebpdemux.so.2.0.7;
+/usr/sbin/semanage fcontext --add --type libwebp_lib_t /usr/lib/libwebpdemux.so.2.0.8;
 /usr/sbin/semanage fcontext --add --type libwebp_lib_t /usr/lib/libwebpdemux.so.2;
 /usr/sbin/semanage fcontext --add --type libwebp_lib_t /usr/lib/libwebpmux.so.3.0.6;
+/usr/sbin/semanage fcontext --add --type libwebp_lib_t /usr/lib/libwebpmux.so.3.0.7;
 /usr/sbin/semanage fcontext --add --type libwebp_lib_t /usr/lib/libwebpmux.so.3;
