a610a482f4c9900f1fe174c0d88b58225b547134
noatsecure <noatsecure@protonmail.com>
2021-10-02 10:43:20 -0600

    Added missing library

--- applications-labels/lua-libs_label/label.sh
+++ applications-labels/lua-libs_label/label.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+/usr/sbin/semanage fcontext --add --type lua-libs_t /usr/lib/liblua-5.1.so;
 /usr/sbin/semanage fcontext --add --type lua-libs_t /usr/lib/liblua-5.3.so;
 /usr/sbin/semanage fcontext --add --type lua-libs_t /usr/lib/liblua-5.4.so;
