b553fb6c95b57782476416e9a65d9e614447c425
noatsecure <noatsecure@protonmail.com>
2021-10-24 12:15:44 -0600

    Added SELinux policy (label only)

---
+++ applications-labels/whois-nls_label/label.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+/usr/sbin/semanage fcontext --add --type whois-nls_usr_t "/usr/share/locale/cs/LC_MESSAGES/whois.mo";
+/usr/sbin/semanage fcontext --add --type whois-nls_usr_t "/usr/share/locale/da/LC_MESSAGES/whois.mo";
+/usr/sbin/semanage fcontext --add --type whois-nls_usr_t "/usr/share/locale/de/LC_MESSAGES/whois.mo";
+/usr/sbin/semanage fcontext --add --type whois-nls_usr_t "/usr/share/locale/el/LC_MESSAGES/whois.mo";
+/usr/sbin/semanage fcontext --add --type whois-nls_usr_t "/usr/share/locale/es/LC_MESSAGES/whois.mo";
+/usr/sbin/semanage fcontext --add --type whois-nls_usr_t "/usr/share/locale/eu/LC_MESSAGES/whois.mo";
+/usr/sbin/semanage fcontext --add --type whois-nls_usr_t "/usr/share/locale/fi/LC_MESSAGES/whois.mo";
+/usr/sbin/semanage fcontext --add --type whois-nls_usr_t "/usr/share/locale/fr/LC_MESSAGES/whois.mo";
+/usr/sbin/semanage fcontext --add --type whois-nls_usr_t "/usr/share/locale/it/LC_MESSAGES/whois.mo";
+/usr/sbin/semanage fcontext --add --type whois-nls_usr_t "/usr/share/locale/ja/LC_MESSAGES/whois.mo";
+/usr/sbin/semanage fcontext --add --type whois-nls_usr_t "/usr/share/locale/pl/LC_MESSAGES/whois.mo";
+/usr/sbin/semanage fcontext --add --type whois-nls_usr_t "/usr/share/locale/pt_BR/LC_MESSAGES/whois.mo";
+/usr/sbin/semanage fcontext --add --type whois-nls_usr_t "/usr/share/locale/ru/LC_MESSAGES/whois.mo";
+/usr/sbin/semanage fcontext --add --type whois-nls_usr_t "/usr/share/locale/zh_CN/LC_MESSAGES/whois.mo";
