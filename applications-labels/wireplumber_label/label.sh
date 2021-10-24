5654d71274ff9071442aadaa2940cf7efd996022
noatsecure <noatsecure@protonmail.com>
2021-10-24 12:15:45 -0600

    Added SELinux policy (label only)

---
+++ applications-labels/wireplumber_label/label.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+/usr/sbin/semanage fcontext --add --type wireplumber_bin_t "/usr/bin/wireplumber";
+/usr/sbin/semanage fcontext --add --type wireplumber_bin_t "/usr/bin/wpctl";
+/usr/sbin/semanage fcontext --add --type wireplumber_bin_t "/usr/bin/wpexec";
+/usr/sbin/semanage fcontext --add --type wireplumber_lib_t "/usr/lib/systemd/user/wireplumber.service";
+/usr/sbin/semanage fcontext --add --type wireplumber_lib_t "/usr/lib/systemd/user/wireplumber@.service";
+/usr/sbin/semanage fcontext --add --type wireplumber_usr_t "/usr/share/licenses/wireplumber(.*)?";
+/usr/sbin/semanage fcontext --add --type wireplumber_usr_t "/usr/share/wireplumber(.*)?";
