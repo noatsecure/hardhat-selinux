ee5300b3959ce0fd03232a2ac56f59e08a0b1d5f
noatsecure <noatsecure@protonmail.com>
2021-12-23 17:00:00 -0700

    Updated the label to use regex

--- applications/brave-browser/label.sh
+++ applications/brave-browser/label.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 /usr/sbin/semanage fcontext --add --type brave-browser_exec_t /opt/brave.com/brave/brave-browser;
-/usr/sbin/semanage fcontext --add --type brave-browser_exec_t /usr/bin/brave-browser;
-/usr/sbin/semanage fcontext --add --type brave-browser_exec_t /usr/bin/brave-browser-stable;
+/usr/sbin/semanage fcontext --add --type brave-browser_exec_t "/usr/bin/brave-browser(.*)";
